cdc7968cb95d189f684047a1b5663233a2e8a06a
Hannah Angle <hannahcangle@gmail.com>
2020-08-01 04:29:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,9 +7,8 @@ def batch_badge_creator(attendees)
     badge_maker(name)
   end
 end
-  def assign_rooms(name, room_number)
-    counter = 1 
-    names.each do |name|
+  def assign_rooms(attendees)
+    
     puts "Hello, #{name}! You'll be assigned to room #{room_number}!"
   end
 end
