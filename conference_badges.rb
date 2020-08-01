12d929d7ba8b390973288ebf1b3dc98b0f2f3beb
Hannah Angle <hannahcangle@gmail.com>
2020-08-01 04:18:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |attendee|
-    puts "#{attendees}"
+  attendees.collect do |name|
+    badge_maker(name)
   end
   
